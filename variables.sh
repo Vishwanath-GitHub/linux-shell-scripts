27674b569ba6cde1e9cd7e6b2240ed104da93b09
vishu <vishu2498@gmail.com>
2020-07-17 17:53:39 +0530

    system and user defined variables

---
+++ variables.sh
@@ -0,0 +1,15 @@
+echo $BASH # system variable
+echo $BASH_VERSION # system variable
+echo $PWD # system variable
+echo $HOME # system variable
+echo $USERNAME # system variable
+
+#user-defined variables
+var1=bhopal
+echo the city is $var1
+
+var2=test
+echo '$var4' # will not print value of var4
+
+VALUE=48645 # Capital user-defined variables are totally OK
+echo $VALUE
