c09871850151a0dcbe960954432f6d8e32381777
vishu <vishu2498@gmail.com>
2020-07-19 21:01:51 +0530

    adding basic calculator function

--- arithmetic-operations.sh
+++ arithmetic-operations.sh
@@ -24,3 +24,34 @@ echo $(expr $value1 - $value2)  # subtracting values
 echo $(expr $value1 \* $value2)  # multiplying values # '\*' symbol needs to be used with 'expr' to perform '*'
 echo $(expr $value1 / $value2)  # dividing values
 echo $(expr $value1 % $value2)  # modulus operation
+echo
+
+
+# performing arithmetic operations on float (decimal) values
+# "bc" stands for basic calculator (its a language in itself)
+echo "32.89+51.75" | bc # pipe opertors sends everything mentioned with 'echo' to 'bc' and 'bc' performs calculation
+echo "32.89-51.75" | bc
+echo "32.89*51.75" | bc
+echo "32.89/51.75" | bc # this output will still be in 'int'
+echo "scale=5;32.89/51.75" | bc # 'scale' will tell 'bc' to output value with number of decimal places while division
+echo "32.89%51.75" | bc
+echo
+
+# using variables with 'bc'
+value3=96.25
+value4=23.48
+echo "$value3+$value4" | bc 
+echo "$value3-$value4" | bc
+echo "$value3*$value4" | bc
+echo "scale=5;$value3/$value4" | bc # 'scale' will tell 'bc' to output value with number of decimal places while division
+echo "$value3%$value4" | bc
+echo
+
+# performing square-root
+value5=31
+echo "scale=3;sqrt($value5)" | bc -l # '-l' is used for calling math library that contains sqrt() function
+echo
+
+# power of values
+value6=5
+echo "scale=3;($value6)^5" | bc -l
