d5e75688d3e69848c9e45430e98268b08e3c3f21
vishu <vishu2498@gmail.com>
2020-08-05 18:14:26 +0530

    debugging shell script using command line

---
+++ Debugging/debugging-shell-script.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# use this command while running this script: 'bash -x ./debugging-shell-script.sh'
+# it will show the entire working process of running this script
+
+for i in {1..7}; do
+	echo $i
+	sleep 3
+done
